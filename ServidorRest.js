763301e8c0ea1ca3cde39cc18029278bed13843f
oscarblanq <oscarblanq@gmail.com>
2019-11-21 19:50:09 +0100

    -

--- ServidorRest.js
+++ ServidorRest.js
@@ -20,7 +20,13 @@ const routerBasurero = require('./reglasREST/usuarios/basurero.router')
  * @params: req: Object, res: Object, next
  * @return: void
  ***************************************************/
-
+app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  })
 /*
     Carlos Tortosa
 
