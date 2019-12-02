241464590d66044ef36ddcac72e3f7c91af4a44d
CTortosaM <cartormi@epsg.upv.es>
2019-12-02 23:48:47 +0100

    Metodo de kriging +Creacion test

---
+++ Logica/test/testKriging.js
@@ -0,0 +1,30 @@
+const mocha = require('mocha');
+const Logica = require('../Logica');
+const assert = require('assert');
+
+const laLogica = new Logica('copiaDeLaBD.db');
+
+let medida1 = {
+    latitud: 1,
+    longitud: 2,
+    valorMedido: 3
+}
+
+
+let medida2 = {
+    latitud: 2,
+    longitud: -1,
+    valorMedido: 6
+}
+
+
+
+let medida3 = {
+    latitud: 1.5,
+    longitud: 2.3,
+    valorMedido: 2
+}
+
+
+let medidas = [medida1, medida2, medida3];
+console.log(laLogica.kriging(medidas,{longitud:2.3, latitud:1.5}));
