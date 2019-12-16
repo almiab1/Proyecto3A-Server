52362654bf358455ec0c1c0103c526709fef3cea
CTortosaM <cartormi@epsg.upv.es>
2019-12-16 13:21:06 +0100

    Reglas rest con metodo de calidad del aire para camiones

--- Logica/ServidorRest.js
+++ Logica/ServidorRest.js
@@ -21,48 +21,48 @@ const routerBasurero = require('./reglasREST/usuarios/basurero.router')
  * @params: req: Object, res: Object, next
  * @return: void
  ***************************************************/
-app.use( (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-})
-/*
-    Carlos Tortosa
+app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+    })
+    /*
+        Carlos Tortosa
 
-*/
+    */
 
 //------------------------------------------------------------------------------------------
 // constructor objeto LogicaDeNegocio
 //------------------------------------------------------------------------------------------
-LogicaDeNegocio = new Logica('./Logica/baseDeDatos.db');
+LogicaDeNegocio = new Logica('./SQL/baseDeDatos.db');
 
 
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json()); //Auto parsea a objeto el body de los requests
 
 app.use('/admin', LogicaDeNegocio.autentificarUsuario)
 app.use('/basurero', LogicaDeNegocio.autentificarUsuario)
 
 // enrrutadores
-app.use('/',routerCiudadano)
-app.use('/admin',routerAdministrador)
-app.use('/basurero',routerBasurero)
+app.use('/', routerCiudadano)
+app.use('/admin', routerAdministrador)
+app.use('/basurero', routerBasurero)
 
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
-app.get('/', function(req,res){
-    //res.sendFile(path.join(__dirname,'/HTML/index.html'));
-    res.send({"message": "Server Worskking"})
-})
-//------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------
+app.get('/', function(req, res) {
+        //res.sendFile(path.join(__dirname,'/HTML/index.html'));
+        res.send({ "message": "Server Worskking" })
+    })
+    //------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------
 app.get('/prueba', (req, res) => {
-console.log("REQ!  "+ req);
-res.status(200).send({mensaje: 'yas'});
+    console.log("REQ!  " + req);
+    res.status(200).send({ mensaje: 'yas' });
 });
 
 
@@ -70,12 +70,12 @@ res.status(200).send({mensaje: 'yas'});
 // puertos
 //------------------------------------------------------------------------------------------
 let PUERTO;
-if(process.env.PORT === undefined) {
+if (process.env.PORT === undefined) {
     PUERTO = 8080;
 } else {
     PUERTO = process.env.PORT;
 }
 
-app.listen(PUERTO, function(){
+app.listen(PUERTO, function() {
     console.log('Server conectado en puerto ' + PUERTO);
 });
