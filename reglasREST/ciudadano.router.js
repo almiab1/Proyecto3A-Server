e3ae4b7ecec0af67788ea1fbfe43002670685a1e
CTortosaM <cartormi@epsg.upv.es>
2019-12-20 11:49:18 +0100

    Update ciudadano.router.js

--- reglasREST/ciudadano.router.js
+++ reglasREST/ciudadano.router.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 
 const jsdom = require('jsdom')
 const {
-  JSDOM
+    JSDOM
 } = jsdom
 
 //------------------------------------------------------------------------------------------
@@ -19,103 +19,103 @@ const router = express.Router()
 // /getUltimaMedida
 //------------------------------------------------------------------------------------------
 router.get('/getUltimaMedida', function(req, res) {
-  LogicaDeNegocio.getUltimaMedida(function(err, resultado) {
-    if (err) {
-      if (err == 'Sin resultados') {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(500);
-      }
-    } else {
-      res.send(JSON.stringify(resultado)).status(200);
-    }
-  });
-}) //getUltimaMedida
-//------------------------------------------------------------------------------------------
-// /getAllMedidas
-//------------------------------------------------------------------------------------------
+        LogicaDeNegocio.getUltimaMedida(function(err, resultado) {
+            if (err) {
+                if (err == 'Sin resultados') {
+                    res.sendStatus(404);
+                } else {
+                    res.sendStatus(500);
+                }
+            } else {
+                res.send(JSON.stringify(resultado)).status(200);
+            }
+        });
+    }) //getUltimaMedida
+    //------------------------------------------------------------------------------------------
+    // /getAllMedidas
+    //------------------------------------------------------------------------------------------
 router.get('/getAllMedidas', (req, res) => {
-  LogicaDeNegocio.getAllMedidas(function(err, resultado) {
-    if (err) {
-      if (err == 'Sin resultados') {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(500);
-      }
-    } else {
-      res.send(JSON.stringify(resultado)).status(200);
-    }
-  });
-}) // getAllMedidas
-//------------------------------------------------------------------------------------------
-// /getAllOzono
-//------------------------------------------------------------------------------------------
+        LogicaDeNegocio.getAllMedidas(function(err, resultado) {
+            if (err) {
+                if (err == 'Sin resultados') {
+                    res.sendStatus(404);
+                } else {
+                    res.sendStatus(500);
+                }
+            } else {
+                res.send(JSON.stringify(resultado)).status(200);
+            }
+        });
+    }) // getAllMedidas
+    //------------------------------------------------------------------------------------------
+    // /getAllOzono
+    //------------------------------------------------------------------------------------------
 router.get('/getAllOzono', (req, res) => {
-  LogicaDeNegocio.getAllOzono(function(err, resultado) {
-    if (err) {
-      if (err == 'Sin resultados') {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(500);
-      }
-    } else {
-      res.send(JSON.stringify(resultado)).status(200);
-    }
-  });
-}) // getAllOzono
+        LogicaDeNegocio.getAllOzono(function(err, resultado) {
+            if (err) {
+                if (err == 'Sin resultados') {
+                    res.sendStatus(404);
+                } else {
+                    res.sendStatus(500);
+                }
+            } else {
+                res.send(JSON.stringify(resultado)).status(200);
+            }
+        });
+    }) // getAllOzono
 
 
 //------------------------------------------------------------------------------------------
 //  POST /login
 //------------------------------------------------------------------------------------------
-router.post('/login', async (req, res) => {
+router.post('/login', async(req, res) => {
 
-  var usuario = req.body.idUsuario
-  var contrasenya = req.body.contrasenya
+    var usuario = req.body.idUsuario
+    var contrasenya = req.body.contrasenya
 
-  // comprueba que hay idUsuario y contrasenya
-  if (!usuario || !contrasenya) {
-    console.log({
-      usuario,
-      contrasenya
-    })
-    res.status(400).send({
-      message: 'idUsuario and contrasenya required'
-    })
-  }
-  await LogicaDeNegocio.verificarUsuario(usuario, contrasenya, (err, resultado) => {
-    if (err) {
-      res.status(401).end();
-      return;
-    } else {
-      try {
-        if (resultado == '') {
-          res.status(404).end();
-          return;
-        }
-        // payload (token)
-        var tokenData = {
-          usuario: usuario,
-          idTipoUsuario: resultado[0].idTipoUsuario
-        }
-        // signing options
-        var signOptions = {
-          issuer: 'iPolution',
-          expiresIn: 60 * 60 * 24
-          // aqui puede meterse un algoritmo de encriptación
-        }
-        // Se genera el token
-        const token = jwt.sign(tokenData, 'privateKey', signOptions)
-        console.log(token)
-        res.status(200).send({
-          token
+    // comprueba que hay idUsuario y contrasenya
+    if (!usuario || !contrasenya) {
+        console.log({
+            usuario,
+            contrasenya
         })
-      } catch (error) {
-        console.error(error)
-        res.status(401).end()
-      }
+        res.status(400).send({
+            message: 'idUsuario and contrasenya required'
+        })
     }
-  });
+    await LogicaDeNegocio.verificarUsuario(usuario, contrasenya, (err, resultado) => {
+        if (err) {
+            res.status(401).end();
+            return;
+        } else {
+            try {
+                if (resultado == '') {
+                    res.status(404).end();
+                    return;
+                }
+                // payload (token)
+                var tokenData = {
+                        usuario: usuario,
+                        idTipoUsuario: resultado[0].idTipoUsuario
+                    }
+                    // signing options
+                var signOptions = {
+                        issuer: 'iPolution',
+                        expiresIn: 60 * 60 * 24
+                            // aqui puede meterse un algoritmo de encriptación
+                    }
+                    // Se genera el token
+                const token = jwt.sign(tokenData, 'privateKey', signOptions)
+                console.log(token)
+                res.status(200).send({
+                    token
+                })
+            } catch (error) {
+                console.error(error)
+                res.status(401).end()
+            }
+        }
+    });
 }); // /login
 
 
@@ -124,10 +124,29 @@ router.post('/login', async (req, res) => {
 //------------------------------------------------------------------------------------------
 router.get('/getMedidasOficiales', (req, res) => {
 
-  // funcion que muestra las medidas oficiales
-  LogicaDeNegocio.getMedidasOficialeslol(req, res);
+        // funcion que muestra las medidas oficiales
+        LogicaDeNegocio.getMedidasOficialeslol(req, res);
 
-}) // /getMedidasOficiales
+    }) // /getMedidasOficiales
+
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------
+//  GET /getMedidasDeIntervaloConcreto
+//------------------------------------------------------------------------------------------
+router.get('/getMedidasDeIntervaloConcreto', (req, res) => {
+
+        LogicaDeNegocio.getMedidasDeIntervaloConcreto(req.body, function(err, res) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(res);
+            }
+        });
+
+    }) // /getMedidasOficiales
 
 
 //------------------------------------------------------------------------------------------
